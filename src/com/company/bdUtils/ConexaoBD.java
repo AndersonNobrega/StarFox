3b88405ac37d7888ce3743995cc5f7125d64241a
Reb0rn <anderson.nobrega62@gmail.com>
2017-12-22 16:13:28 -0300

    Adicionando interface inicial (Sem funcionalidade) e inicio da conexão com sqlite

---
+++ src/com/company/bdUtils/ConexaoBD.java
@@ -0,0 +1,19 @@
+package com.company.bdUtils;
+
+import java.sql.*;
+
+public class ConexaoBD {
+
+    private static final String SQCONN = "jdbc:sqlite:login.db";
+
+    public static Connection verificaConexao() throws SQLException {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            return DriverManager.getConnection(SQCONN);
+        } catch (ClassNotFoundException error) {
+            error.printStackTrace();
+            return null;
+        }
+    }
+
+}
---
+++ src/com/company/login/LoginApp.java
@@ -0,0 +1,31 @@
+package com.company.login;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginApp extends Application{
+
+    public void start(Stage stage){
+        try {
+            Parent root = (Parent) FXMLLoader.load(getClass().getResource("login.fxml"));
+
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.setTitle("StarFox");
+            stage.show();
+        } catch (IOException error) {
+            System.err.println("Arquivo FXML não encontrado");
+            System.exit(1);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
